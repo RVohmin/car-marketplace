4e1af725b81d4e150f188972287e5a711f2712de
Роман Александрович Вохмин <rvokhmin@alfabank.ru>
2023-06-21 15:50:33 +0300

    m3l1 - add spring application

--- car-marketplace-app-spring/src/main/kotlin/ru/vohmin/marketplace/springapp/config/SerializationConfiguration.kt
+++ car-marketplace-app-spring/src/main/kotlin/ru/vohmin/marketplace/springapp/config/SerializationConfiguration.kt
@@ -1,16 +1,19 @@
-package ru.vohmin.marketplace.springapp.config
+//package ru.vohmin.marketplace.springapp.config
+//
+//import kotlinx.serialization.json.Json
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.http.converter.json.KotlinSerializationJsonHttpMessageConverter
+//
+//@Configuration
+//class SerializationConfiguration {
+//    @Bean
+//    fun messageConverter(): KotlinSerializationJsonHttpMessageConverter {
+//        return KotlinSerializationJsonHttpMessageConverter(Json {
+//            ignoreUnknownKeys = true
+//        })
+//    }
+//}
 
-import kotlinx.serialization.json.Json
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.http.converter.json.KotlinSerializationJsonHttpMessageConverter
 
-@Configuration
-class SerializationConfiguration {
-    @Bean
-    fun messageConverter(): KotlinSerializationJsonHttpMessageConverter {
-        return KotlinSerializationJsonHttpMessageConverter(Json {
-            ignoreUnknownKeys = true
-        })
-    }
-}
+// оставляю как пример, когда понадобится
